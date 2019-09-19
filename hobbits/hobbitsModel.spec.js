10f4573ad36f079021184ebf4412b8e9826383e2
Adnan <adnan.chowdhury@outlook.com>
2019-09-19 13:04:40 -0500

    testing done

---
+++ hobbits/hobbitsModel.spec.js
@@ -0,0 +1,29 @@
+const Hobbits = require("./hobbitsModel");
+const db = require("../data/dbConfig");
+
+describe("hobbits model", () => {
+  beforeEach(async () => {
+    await db("hobbits").truncate();
+  });
+  it("should set environment to testing", () => {
+    expect(process.env.DB_ENV).toBe("testing");
+  });
+  describe("insert()", () => {
+    it("should insert hobbits into the DB", async () => {
+      await Hobbits.insert({ name: "Gaffer" });
+
+      const hobbits = await db("hobbits");
+
+      expect(hobbits).toHaveLength(1);
+    });
+    it("should insert hobbits into the DB", async () => {
+      const [id] = await Hobbits.insert({ name: "Gaffer" });
+
+      let hobbit = await db("hobbits")
+        .where({ id })
+        .first();
+
+      expect(hobbit.name).toBe("Gaffer");
+    });
+  });
+});
